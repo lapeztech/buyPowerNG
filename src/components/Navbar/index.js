2869c660fc114c1bf073fc8f39b22dcb0e4e0624
ifeanyi <ifeanyiokoye2018@gmail.com>
2020-07-07 19:28:29 +0100

    finalle

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,7 +9,7 @@ function Navbar() {
             <div className="col-md-10">
                 <nav class="navbar navbar-expand-lg navbar-light">
                     <img src={logo} alt="logo" />
-                    <a class="navbar-brand" href="#"><img src="./../../assets/images/logo.png" alt="" /></a>
+                    <a class="navbar-brand" href="/"><img src="./../../assets/images/logo.png" alt="" /></a>
 
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,28 +19,28 @@ function Navbar() {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Buy Electricity</a>
+                                <a class="nav-link" href="/">Buy Electricity</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Merchant</a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">FAQ</a>
+                                <a class="nav-link" href="/">Merchant</a>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Contact Us</a>
+                                <a class="nav-link" href="/">FAQ</a>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="#">14 Hour Support</a>
+                                <a class="nav-link" href="/">Contact Us</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="/l">14 Hour Support</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">0908-749-3044</a>
+                                <a class="nav-link" href="/">0908-749-3044</a>
                             </li>
                             <li class="nav-item button">
-                                <a class="nav-link" href="#">LOGIN</a>
+                                <a class="nav-link" href="/">LOGIN</a>
                             </li>
 
 
@@ -53,7 +53,7 @@ function Navbar() {
 
 
 
-        </div>
+        </div >
     )
 }
 
